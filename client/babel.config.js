1d423087c3d51dc93eb7ac3e89c18ee4774c48b5
Jasper August Tootsi <jasperaugusttootsi@gmail.com>
2020-03-26 13:00:38 +0200

    Babel setup

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,6 +1,10 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset', 'module:react-native-dotenv'],
+    presets: [
+      'module:metro-react-native-babel-preset',
+      'module:react-native-dotenv',
+      'module:babel-preset-expo',
+    ],
   };
 };
